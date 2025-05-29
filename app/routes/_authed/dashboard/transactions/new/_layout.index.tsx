acc1774c56280a0c8a7c0acd388172b3f91f9b9a
Nenad <nenadmihajlovic1402@gmail.com>
2025-05-29 21:18:50 +0200

    Started work on transaction form card

--- app/routes/_authed/dashboard/transactions/new/_layout.index.tsx
+++ app/routes/_authed/dashboard/transactions/new/_layout.index.tsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { TransactionForm } from '@/components/transaction-form';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authed/dashboard/transactions/new/_layout/')({
-  component: RouteComponent,
-})
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/_authed/dashboard/transactions/new/"!</div>
+	return (
+		<Card className='max-w-screen-md mt-4'>
+			<CardHeader>
+				<CardHeader>
+					<CardTitle>New Transaction</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<TransactionForm />
+				</CardContent>
+			</CardHeader>
+		</Card>
+	);
 }
