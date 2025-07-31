f5a6b58b465bea2b03c6be1b89f05952c6060eb2
Nenad <nenadmihajlovic1402@gmail.com>
2025-07-31 21:26:48 +0200

    Update

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -48,7 +48,7 @@ const useFormField = () => {
   const fieldState = getFieldState(fieldContext.name, formState)
 
   if (!fieldContext) {
-    throw new Error("useFormField should be used within <FormField>")
+    throw new Error("useFormField should be used from within <FormField>")
   }
 
   const { id } = itemContext
